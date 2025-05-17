bc53dafb36f2715db5ab537e1057c69dd4321eee
Ferdinan7 <ferna550spen@gmail.com>
2025-05-16 20:59:12 -0600

    ✨ fix: prevenir múltiples instancias de PrismaClient en el entorno de desarrollo
    #prisma.ts: Declare prisma como una variable global para evitar crear múltiples instancias de PrismaClient durante el desarrollo. Asignar prisma a global.prisma si existe, en caso contrario crear una nueva instancia. Este cambio mejora el rendimiento y evita posibles problemas de conexión manejando tambien la desconexion.
    #schema.prisma: Añadi también directUrl a  para la configuración de conexion al igual que migracione

--- src/shared/models/prisma.ts
+++ src/shared/models/prisma.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+declare global {
+  // Esto le dice a TypeScript que global.prisma puede existir
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma = global.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 export default prisma;
